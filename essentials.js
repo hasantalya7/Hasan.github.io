a40594555599ebdf01bfb173373bdad3e0d1e993
Hasan Serdar <64413598+hasantalya7@users.noreply.github.com>
2022-09-02 11:07:36 +0300

    essentials.js uploaded

---
+++ essentials.js
@@ -0,0 +1,7 @@
+document.getElementById("myHead").innerHTML =
+	"<span id='headerText'><h1>Hasan Serdar</h1></span>"
+	+ "<span id='headerSubtext' style = 'font-size: 13px;'><p>Asst. Professor, PhD.</br>Necmettin Erbakan University</br>Seydisehir Ahmet Cengiz Faculty of Engineering</br>Department of Computer Engineering</br>Head of Computer Hardware Department</p></span>";
+
+document.getElementById("myFooter").innerHTML =
+	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " Hasan Serdar. All"
+	+ " rights reserved.</p>"
